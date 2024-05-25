767395d3beca75032636092cca6ac386799c03c0
Raí Rafael <rairafaelss@gmail.com>
2024-05-25 18:57:08 -0300

    Classe UserResource findById

---
+++ src/main/java/br/com/dev/api/resources/UserResource.java
@@ -0,0 +1,18 @@
+package br.com.dev.api.resources;
+
+import br.com.dev.api.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(new User(1, "Raí", "rai@email.com", "123"));
+    }
+}
