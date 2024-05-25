96c2ed00dd0ba260b13275d9febfcd4d827d3b0c
Raí Rafael <rairafaelss@gmail.com>
2024-05-25 19:25:58 -0300

    interface user repository

---
+++ src/main/java/br/com/dev/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.dev.api.repositories;
+
+import br.com.dev.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
