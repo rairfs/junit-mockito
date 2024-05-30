588d8a2fb7c1196911d24a8d39ddbc67d2394fae
Raí Rafael <rairafaelss@gmail.com>
2024-05-30 14:38:03 -0300

    Teste para classe UserService metodo update com excessao

--- src/test/java/br/com/dev/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/dev/api/services/impl/UserServiceImplTest.java
@@ -117,7 +117,18 @@ class UserServiceImplTest {
     }
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+
+        User response = service.update(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+
     }
 
     @Test
